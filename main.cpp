adcf9cd948b02f800a90f352742606c781b49a82
ebrown8534 <elliott.brown@evbpc.com>
2012-12-09 14:42:09 -0500

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <SFML\System.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\Audio.hpp>
+#include <SFML\Network.hpp>
+
+int main()
+{
+	std::cout << "Test";
+
+#if DEBUG
+	system("pause");
+#endif
+
+	return 0;
+}
