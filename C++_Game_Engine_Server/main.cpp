696f263019b73a59cda5df999a21031fd7e15943
ebrown8534 <elliott.brown@evbpc.com>
2012-12-09 15:28:20 -0500

    Re-built Initial Commit. (Will clean up how the Git would have looked from the original commit.)

---
+++ C++_Game_Engine_Server/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Project: C++_Game_Engine_Server
+ * Author: Elliott Brown (EBrown8534)
+ * Date Created: 9 December 2012
+ * Purpise: To provide a Server for a 2D C++ Game Engine
+ */
+
+#include <iostream>
+#include <SFML\Audio.hpp>
+#include <SFML\Config.hpp>
+#include <SFML\Graphics.hpp>
+#include <SFML\Network.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Window.hpp>
+
+int main()
+{
+	std::cout << "Test" << std::endl;
+
+#if DEBUG
+	system("pause");
+#endif
+
+	return 0;
+}
